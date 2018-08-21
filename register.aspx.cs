39c8e21c9c76f5ebe543d406902184ccff9cfae0
kdncode <kdncode@gmail.com>
2018-08-20 23:25:52 -0500

    Update register.aspx.cs

--- register.aspx.cs
+++ register.aspx.cs
@@ -10,8 +10,7 @@ using System.Configuration;
 
 public partial class register : System.Web.UI.Page {
 
-    protected void Page_Load(object sender, EventArgs e)
-    {
+    protected void Page_Load(object sender, EventArgs e) {
 
     }
 
