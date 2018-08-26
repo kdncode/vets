a85ca839449703cb8b9f1ca4ff9bc531aa05aea6
kdncode <kdncode@gmail.com>
2018-08-26 00:23:27 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 $(function() {
-	$(".btn").click(function() {
-		$(".form-signin").toggleClass("form-signin-left");
+    $(".btn").click(function() {
+    $(".form-signin").toggleClass("form-signin-left");
     $(".form-signup").toggleClass("form-signup-left");
     $(".frame").toggleClass("frame-long");
     $(".signup-inactive").toggleClass("signup-active");
