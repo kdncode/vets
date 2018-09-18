1b1d2edccd727808874cdad2febe7618f316608a
kdncode <kdncode@gmail.com>
2018-09-17 23:33:10 -0500

    Update products.aspx.cs

--- products.aspx.cs
+++ products.aspx.cs
@@ -48,26 +48,26 @@ public partial class products : System.Web.UI.Page {
     {
         if (e.CommandName == "GioHang") {
             
-                int intidSP = int.Parse(DataList1.DataKeys[e.Item.ItemIndex].ToString());
-                DataTable Temp = db.GetTable("select P_Name, P_Price from products Where P_ID='"+intidSP+"'");
-                string strTenSP = Temp.Rows[0][0].ToString();
-                float flGia = int.Parse(Temp.Rows[0][1].ToString());
-                int intSoLuong = 1;
-                //Add vao gio hang
+            int intidSP = int.Parse(DataList1.DataKeys[e.Item.ItemIndex].ToString());
+            DataTable Temp = db.GetTable("select P_Name, P_Price from products Where P_ID='"+intidSP+"'");
+            string strTenSP = Temp.Rows[0][0].ToString();
+            float flGia = int.Parse(Temp.Rows[0][1].ToString());
+            int intSoLuong = 1;
+            //Add vao gio hang
 
-                foreach (DataRow row in tbGioHang.Rows)
-                {//Kiem tr neu mat hang da co roi thi tang so luong len 1
-                    if ((int)row["idSP"] == intidSP) {
-                        row["SoLuong"] = (int)row["SoLuong"] + 1;
-                        goto GioHang;
-                    }
+            foreach (DataRow row in tbGioHang.Rows)
+            {//Kiem tr neu mat hang da co roi thi tang so luong len 1
+                if ((int)row["idSP"] == intidSP) {
+                    row["SoLuong"] = (int)row["SoLuong"] + 1;
+                    goto GioHang;
                 }
-            
-                tbGioHang.Rows.Add(intidSP, strTenSP, flGia, intSoLuong);
-                GioHang:
-                Session["GioHang"] = tbGioHang;      
+            }
 
-                Response.Write("<script>alert('The item has been successfully added to cart')</script>");          
+            tbGioHang.Rows.Add(intidSP, strTenSP, flGia, intSoLuong);
+            GioHang:
+            Session["GioHang"] = tbGioHang;      
+
+            Response.Write("<script>alert('The item has been successfully added to cart')</script>");          
         }
 }
     protected void ButtonViewCart_Click(object sender, EventArgs e) {
