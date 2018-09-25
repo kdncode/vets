112654741426ff2fa2454e2f0b67bc2844ef50b4
kdncode <kdncode@gmail.com>
2018-09-24 22:31:10 -0500

    Update welcome.aspx.cs

--- welcome.aspx.cs
+++ welcome.aspx.cs
@@ -7,8 +7,7 @@ using System.Web.UI.WebControls;
 
 public partial class welcome : System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
-        if (Session["UserName"] != null)
-        {
+        if (Session["UserName"] != null) {
             lblWelcome.Text = "Hello " + Session["Username"].ToString();
             lblWelcome.Visible = true;
         }
